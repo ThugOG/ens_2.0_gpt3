c678cbdb8ecea11fde1fd23c252e985db9e9bc7c
Blacklight34 <sahilgthakur1@gmail.com>
2023-03-11 15:10:12 +0530

    fixed api cors issue

--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({extended: false}))
 app.use("/getResponse", router)
+app.get("/", (req, res)=>{
+    res.status(200).json({
+        status: "Dont worry i gotcha"
+    })
+})
 app.listen(PORT, ()=> {
     console.log(`App running at ${PORT}`)
 })
